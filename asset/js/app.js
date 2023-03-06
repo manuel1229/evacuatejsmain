122d906b9460a1fb28cdc6449134245f7859c404
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2023-03-06 22:01:47 +0800

    back to top

--- asset/js/app.js
+++ asset/js/app.js
@@ -7,3 +7,24 @@ function logout() {
 
 
 }
+
+/*============karl js=============*/
+const backToTop = document.getElementById("back-to-top");
+
+if (typeof backToTop !== 'undefined' && backToTop !== null) {
+   window.addEventListener("scroll", function () {
+     if (window.scrollY >= 150) {
+       backToTop.classList.add("visible");
+     } else {
+       backToTop.classList.remove("visible");
+     }
+   });
+   backToTop.addEventListener("click", function () {
+     window.scrollTo({
+       top: 0,
+       behavior: 'smooth'
+     });
+   });
+ }
+
+/*===========END==============*/
